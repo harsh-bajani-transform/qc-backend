47526cee0d0c2ad3578ac5f45473452fd746280c
harsh-bajani-transform <harsh.bajani@transformsolution.net>
2026-01-09 12:08:35 +0530

    update env.ts to include latest db envs of mysql of cloud

--- config/env.ts
+++ config/env.ts
@@ -4,7 +4,8 @@ config({ path: `.env` });
 
 export const {
   PORT, NODE_ENV, SERVER_URL,
-  DB_URI,
   JWT_SECRET, JWT_EXPIRES_IN,
-  EMAIL_USER, EMAIL_PASSWORD
+  EMAIL_USER, EMAIL_PASSWORD,
+  DB_HOST, DB_PORT, DB_DATABASE,
+  DB_USERNAME, DB_PASSWORD
 } = process.env;
